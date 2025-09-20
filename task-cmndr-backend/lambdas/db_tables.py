ca266ae7addad90188a4bc3a67f13d38dd6d7712
grossj0816 <juwangross0815@gmail.com>
2025-09-20 08:07:44 -0400

    completed code for db_tables.create_task_cmndr_db_tables()

--- task-cmndr-backend/lambdas/db_tables.py
+++ task-cmndr-backend/lambdas/db_tables.py
@@ -18,12 +18,14 @@ def create_task_cmndr_db_tables(event, context):
                 print("Connected to MySQL Server version:", db_info)
 
                 cursor = db.cursor()
-                cursor.execute("CREATE TABLE IF NOT EXISTS ```")
-                
-
+                cursor.execute("CREATE TABLE IF NOT EXISTS `users`(`userId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,`firstName` VARCHAR(75),`lastName` VARCHAR(150),`active` BOOLEAN,`createdBy` VARCHAR(100),`createdDate` DATETIME(0),`lastModifiedBy` VARCHAR(100),`lastModifiedDate` DATETIME(0))")
+                cursor.execute("CREATE TABLE IF NOT EXISTS `new_tasks`(`nTaskId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,`userId` INT NOT NULL,`title` VARCHAR(75),`description` VARCHAR(150),`completed` BOOLEAN,`createdBy` VARCHAR(100),`createdDate` DATETIME(0),`lastModifiedBy` VARCHAR(100),`lastModifiedDate` DATETIME(0), FOREIGN KEY(`userId`) REFERENCES `users`(`userId`))")
+                cursor.execute("CREATE TABLE IF NOT EXISTS `archived_tasks`(`setId` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,courseId INT NOT NULL,`setName` VARCHAR(175),`active` BOOLEAN,`createdBy` VARCHAR(100),`createdDate` DATETIME(0),`lastModifiedBy` VARCHAR(100)`lastModifiedDate` DATETIME(0), FOREIGN KEY(`userId`) REFERENCES `users`(`userId`))")
+                print("Table creation has been completed.")
+        except Error as e:
+            print('Error while connecting to MySQL...', e)
     
-
     return{
         "statusCode": 200,
-        "body": json.dumps({"Success": "Database creation process has completed. Double check if you tables were added correctly.")      
+        "body": json.dumps({'Success': 'Database table creation process has been completed. Double check if all tables have been created properly...'})
     }
